e87c578c7031039b85aa7ee0b36f874b18c4fb2f
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-07-31 10:58:09 -0500

    add service for transfers

---
+++ MicroRabbit.MVC/Services/ITransferService.cs
@@ -0,0 +1,9 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+
+namespace MicroRabbit.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transfer);
+    }
+}
---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        public Task Transfer(TransferDto transfer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
