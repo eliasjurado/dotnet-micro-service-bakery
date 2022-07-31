26963d93317d13011e660dd0af01a76046e4c825
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-07-31 11:17:10 -0500

    add controller and its post method

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.Dto;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount,
+            };
+            await _transferService.Transfer(transferDto);
+            return View("Index");
+        }
+    }
+}
