b7a9f3a1fe49d09a18720b056918b32fad13bc3f
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-18 20:16:02 -0500

    add ConsumingInventoryAsync_RemovesStockFromFlourAndButter

--- MicroRabbit.Bakery.Data.Test/BakeryDataTest.cs
+++ MicroRabbit.Bakery.Data.Test/BakeryDataTest.cs
@@ -3,6 +3,7 @@ using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -16,7 +17,9 @@ namespace MicroRabbit.Bakery.Data.Test
         [AvailableButterStockData]
         public void AvailableButterStock_IsEnoughButter(Product data, float value, bool expected)
         {
-            var mockOptions = new DbContextOptionsBuilder<BakeryDbContext>().UseInMemoryDatabase(databaseName: "BakeryDB").Options;
+            var mockOptions = new DbContextOptionsBuilder<BakeryDbContext>()
+                .UseInMemoryDatabase(databaseName: "BakeryDB")
+                .Options;
             var mockContext = new BakeryDbContext(mockOptions);
             var exists = mockContext.Product.Find(data.Id);
             if (exists == null)
@@ -35,7 +38,9 @@ namespace MicroRabbit.Bakery.Data.Test
         [AvailableFlourStockData]
         public void AvailableFlourStock_IsEnoughFlour(Product data, float value, bool expected)
         {
-            var mockOptions = new DbContextOptionsBuilder<BakeryDbContext>().UseInMemoryDatabase(databaseName: "BakeryDB").Options;
+            var mockOptions = new DbContextOptionsBuilder<BakeryDbContext>()
+                .UseInMemoryDatabase(databaseName: "BakeryDB")
+                .Options;
             var mockContext = new BakeryDbContext(mockOptions);
             var exists = mockContext.Product.Find(data.Id);
             if (exists == null)
@@ -50,5 +55,44 @@ namespace MicroRabbit.Bakery.Data.Test
             Assert.Equal(expected, result);
         }
 
+        [Theory]
+        [ConsumingInventoryDataAttribute]
+        public async Task ConsumingInventoryAsync_RemovesStockFromFlourAndButter(Product butter, Product flour, float projectedFlour, float projectedButter, int expectedFlourBalance, int expectedButterBalance)
+        {
+            var mockOptions = new DbContextOptionsBuilder<BakeryDbContext>()
+                .UseInMemoryDatabase(databaseName: "BakeryDB")
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+            var mockContext = new BakeryDbContext(mockOptions, _appSettings);
+
+            var butterExists = mockContext.Product.Find(butter.Id);
+            if (butterExists == null)
+            {
+                mockContext.Product.Add(butter);
+            }
+            mockContext.SaveChanges();
+
+            var flourExists = mockContext.Product.Find(flour.Id);
+            if (flourExists == null)
+            {
+                mockContext.Product.Add(flour);
+            }
+            mockContext.SaveChanges();
+
+            var mockRepository = new BakeryRepository(mockContext);
+            var cancellationTokenSource = new CancellationTokenSource(1000);
+
+            await mockRepository.ConsumingInventoryAsync(projectedFlour, projectedButter, cancellationTokenSource.Token);
+            var resultButterBalance = mockContext.Product.Find(3).Stock;
+            var resultFlourBalance = mockContext.Product.Find(2).Stock;
+
+            Assert.Equal(expectedButterBalance, resultButterBalance);
+            Assert.Equal(expectedFlourBalance, resultFlourBalance);
+        }
+        //[Theory]
+        //public async Task RegisterProductionAsync(float amount, DateTime expirationDate)
+        //{
+
+        //}
     }
 }
