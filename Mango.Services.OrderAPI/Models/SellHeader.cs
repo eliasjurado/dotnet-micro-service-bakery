7bfdb5e7efadbde24e205995ce675f3fd743be2d
diegotangarife <diego.tangarife@globant.com>
2022-08-25 10:39:37 -0500

    Update dto to selldetails

--- Mango.Services.OrderAPI/Models/SellHeader.cs
+++ Mango.Services.OrderAPI/Models/SellHeader.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mango.Services.OrderAPI.Models
 {
     public class SellHeader
     {
+        [Key]
         public int IdSellHeader { get; set; }
         public string UserId { get; set; }
         
@@ -12,7 +14,7 @@ namespace Mango.Services.OrderAPI.Models
         
         public DateTime SellTime { get; set; }
         
-        public List<OrderDetails> SellDetails { get; set; }
+        public List<SellDetails> SellDetails { get; set; }
         
     }
 
