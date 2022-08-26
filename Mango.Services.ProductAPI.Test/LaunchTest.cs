4592819540f6754cd91185b2c7150f18273d1c02
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-26 14:18:54 -0500

    add Startup_IsLaunched

---
+++ Mango.Services.ProductAPI.Test/LaunchTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Mango.Services.ProductAPI.Test
+{
+    public class LaunchTest
+    {
+        [Fact]
+        public void Main_IsLaunched()
+        {
+            string[] args = { };
+            var result = Program.CreateHostBuilder(args);
+            Assert.IsType<HostBuilder>(result);
+        }
+
+        [Fact]
+        public void Startup_IsLaunched()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection()
+                .Build();
+            var result = new Startup(configuration);
+            Assert.IsType<Startup>(result);
+        }
+    }
+}
